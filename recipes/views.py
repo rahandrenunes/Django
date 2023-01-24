bf3b31c09bf1c5ee0bed77dc7710df302aba030d
André Nunes <rah.andre.nunes@gmail.com>
2023-01-24 23:24:42 +0000

    Partials

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     # pass
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'André Nunes',
     })
 
